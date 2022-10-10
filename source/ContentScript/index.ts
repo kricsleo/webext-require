e0d1d6c0c966d60a624e73c504fa4d07aa0edf8b
许圣峰 <kricsleo@163.com>
2022-10-10 15:44:49 +0800

    feat: add require(but not working)

--- source/ContentScript/index.ts
+++ source/ContentScript/index.ts
@@ -1,3 +1,27 @@
-console.log('helloworld from content script');
+// import chalk from 'chalk';
 
-export {};
+async function fetchPkg(pkg: string): Promise<string> {
+  const res = await fetch(`https://cdn.jsdelivr.net/npm/${pkg}`);
+  const code = await res.text();
+  return code;
+}
+
+async function fetchPkgInfo(pkg: string): Promise<Record<string, unknown>> {
+  const res = await fetch(`https://cdn.jsdelivr.net/npm/${pkg}/package.json`);
+  const pkgInfo = await res.json();
+  return pkgInfo;
+}
+
+export async function require(pkg: string): Promise<void> {
+  console.log(`Fetching ${pkg}`);
+  const [code, pkgInfo] = await Promise.all([fetchPkg(pkg), fetchPkgInfo(pkg)]);
+  // eslint-disable-next-line
+  eval(code);
+  console.log(`Done with ${pkgInfo.version}`);
+}
+
+console.log('Started');
+// eslint-disable-next-line
+// @ts-ignore
+// TODO: doesn't work, fix it
+window.requir = require;
