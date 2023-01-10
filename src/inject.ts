a7d24437077712b4474d871319da3a3f3241ab0c
许圣峰 <kricsleo@163.com>
2023-01-10 10:45:13 +0800

    feat: add css inject

--- src/inject.ts
+++ src/inject.ts
@@ -15,39 +15,66 @@ async function fetchPkgInfo(pkg: string): Promise<Record<string, unknown>> {
   return pkgInfo;
 }
 
-let injectError = true
+let injected = false
 async function require(pkg: string): Promise<void> {
   craie.info(red('Fetching'), blue(pkg))
   try {
     const [code, pkgInfo] = await Promise.all([fetchPkg(pkg), fetchPkgInfo(pkg)]);
     const pkgName = pkgInfo.name + '@' + pkgInfo.version
-    const injectedCode = `
-      ${code}
-      ;window.postMessage({ type: 'require_success', data: { pkg: '${pkgName}' } })
-    `
-    injectError = true
-    setTimeout(() => injectError && craie.info(
-        red('⚠️ Failed'),
-        blue(pkgName),
-        craie.red(` \`${pkgName}\` may not support browser.`)
-      ), 
-      300
-    )
-    injectCode(injectedCode)
+    if(isCSS(pkgInfo)) {
+      injectCSS(code)
+      craie.info(red('Required'), blue(pkgName))
+    } else {
+      const injectedCode = `
+        ${code}
+        ;window.postMessage({ type: 'require_success', data: { pkg: '${pkgName}' } })
+      `
+      const prevVars = getWindowVars()
+      injected = false
+      setTimeout(() => {
+        if(injected) {
+          // const addedVars = detectRequiredVar(prevVars)
+          craie.info(red('Required'), blue(pkgName))
+          // if(addedVars.length) {
+          //   craie.info(red('Required'), blue(pkgName), craie.green(` Found added global namespace: ${addedVars.join(',')}`))
+          // } else {
+          //   craie.info(red('Required'), blue(pkgName), craie.red(` But no global namespace for '${pkgName}' found.`))
+          // }
+        } else {
+          craie.info(
+            red('⚠️ Failed'),
+            blue(pkgName),
+            craie.red(` \`${pkgName}\` may not support browser.`)
+          )
+        }
+      }, 300 )
+      injectJS(injectedCode)
+    }
   } catch(e: any) {
     craie.info(red('⚠️ Failed'), blue(pkg), craie.red(' ' + e.message))
   }
 }
 
-function injectCode(code: string) {
+function isCSS(pkgInfo: Record<string, unknown>) {
+  return pkgInfo.main && /.css$/.test(pkgInfo.main as string)
+}
+
+function injectJS(code: string) {
   const script = document.createElement('script')
   script.innerHTML = code
   document.body.appendChild(script)
   script.remove()
 }
 
+function injectCSS(code: string) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.innerHTML = code
+  document.head.appendChild(link);
+}
+
 export function injectRequire(namespace: string = '_require') {
-  injectCode(`
+  injectJS(`
     if(window.${namespace}) {
       window.postMessage({ type: 'conflict', data: { namespace: ${namespace} } })
     } else {
@@ -65,13 +92,21 @@ export function listenRequire() {
       require(pkg)
     }
     if(event.source == window && event.data?.type === 'require_success') {
-      injectError = false
-      const pkg = event.data.data.pkg
-      craie.info(red('Required'), blue(pkg))
+      injected = true
     }
     if(event.source == window && event.data?.type === 'conflict') {
       const namespace = event.data.data.namespace
       craie.info(red('⚠️ Failed'), craie.red(`\`${namespace}\` already existed, won't inject anything.`))
     }
   })
+}
+
+function detectRequiredVar(prevVars: string[]) {
+  const vars = getWindowVars()
+  const addedVars = vars.filter(v => !prevVars.includes(v))
+  return addedVars
+}
+
+function getWindowVars() {
+  return Object.keys(window)
 }
