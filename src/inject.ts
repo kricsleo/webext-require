4ec2ef8744fda21a6e113a98d44157c15412e0bf
许圣峰 <kricsleo@163.com>
2023-01-11 11:55:36 +0800

    refactor: auto detect package version

--- src/inject.ts
+++ src/inject.ts
@@ -11,68 +11,58 @@ enum PKG_TYPE {
 
 async function fetchCDN(url: string) {
   const res = await fetch(url);
-  const pkgType = detectPkgType(res)
+  const type = detectPkgType(res)
   const code = await res.text();
-  return { code, pkgType };
+  return { code, type, version: '', url };
 }
 
-async function fetchPkg(pkg: string) {
-  const res = await fetch(`https://cdn.jsdelivr.net/npm/${pkg}`);
-  const pkgType = detectPkgType(res)
+async function fetchNPM(pkg: string) {
+  const url = `https://cdn.jsdelivr.net/npm/${pkg}`
+  const res = await fetch(url);
+  const type = detectPkgType(res)
+  const version = detectPkgVersion(res)
   const code = await res.text();
-  return { code, pkgType };
+  return { code, type, version, url };
 }
 
-async function fetchPkgInfo(pkg: string): Promise<Record<string, unknown>> {
-  const res = await fetch(`https://cdn.jsdelivr.net/npm/${pkg}/package.json`);
-  const pkgInfo = await res.json();
-  return pkgInfo;
+function isCDN(pkg: string) {
+  return /^https?:/.test(pkg)
 }
 
-function isNpmPkg(pkg: string) {
-  return !/^https?:/.test(pkg)
+function getPkgName(pkg: string) {
+  // match pkgName@version
+  const matches = pkg.match(/(^.+)(@[0-9]+($|\..*))/)
+  return matches ? matches[1] : pkg
 }
 
 let injected = false
 async function require(pkg: string): Promise<void> {
   craie.info(red('Fetching'), blue(pkg))
-  const isNpm = isNpmPkg(pkg)
+  const isCDNPkg = isCDN(pkg)
   try {
-    let code
-    let pkgType
-    let pkgInfo
-    let pkgName
-    if(isNpm) {
-      [{ code, pkgType }, pkgInfo] = await Promise.all([
-        fetchPkg(pkg), 
-        fetchPkgInfo(pkg).catch(() => null)
-      ]);
-      pkgName = pkgInfo ? pkgInfo.name + '@' + pkgInfo.version : pkg + '@unknown'
-    } else {
-      ({ code, pkgType } = await fetchCDN(pkg));
-      pkgName = pkg
-    }
-    switch (pkgType) {
+    const { code, type, version, url } = isCDNPkg ? await fetchCDN(pkg) : await fetchNPM(pkg)
+    const fullPkgName = isCDNPkg ? pkg : getPkgName(pkg) + (version ? `@${version}` : '')
+    switch (type) {
       case PKG_TYPE.JS: {
         const detectVarsCode = `window.postMessage({ type: 'vars', data: { vars: Object.keys(window) } })`
         injectJS(detectVarsCode)
         const injectedCode = `
           ${code}
-          ;window.postMessage({ type: 'require_success', data: { pkg: '${pkgName}', vars: Object.keys(window) } })
+          ;window.postMessage({ type: 'require_success', data: { pkg: '${fullPkgName}', vars: Object.keys(window) } })
         `
         injected = false
         injectJS(injectedCode)
         setTimeout(() => {
           if(!injected) {
-            craie.info( red('⚠️ Failed'), blue(pkgName), craie.red(` \`${pkgName}\` may not support browser`) )
+            craie.info( red('⚠️ Failed'), blue(fullPkgName), craie.red(` \`${fullPkgName}\` may not support browser`) )
           }
         }, 300)
       };break;
       case PKG_TYPE.CSS: {
         injectCSS(code)
-        craie.info(red('Required'), blue(pkgName), craie.blue(' CSS has been injected into current page'))
+        craie.info(red('Required'), blue(fullPkgName), craie.blue(' CSS has been injected into current page'))
       };break;
-      default: craie.info(red('⚠️ Failed'), blue(pkgName), craie.red(` No supported content type auto-detected`))
+      default: craie.info(red('⚠️ Failed'), blue(fullPkgName), craie.red(` No supported content type auto-detected: ${url}`))
     }
   } catch(e: any) {
     craie.info(red('⚠️ Failed'), blue(pkg), craie.red(' ' + e.message))
@@ -88,6 +78,12 @@ function detectPkgType(reponse: Response) {
   } else {
     return PKG_TYPE.OTHER
   }
+}
+
+function detectPkgVersion(reponse: Response) {
+  // header specified by https://cdn.jsdelivr.net
+  const version = reponse.headers.get('x-jsd-version')
+  return version
 }
 
 function injectJS(code: string) {
@@ -119,7 +115,8 @@ export function listenRequire() {
   let prevVars = []
   window.addEventListener("message", event => {
     if(event.source == window && event.data?.type === 'require') {
-      const pkg = event.data.data.pkg
+      const pkgArg = Array.isArray(event.data.data.pkg) ? event.data.data.pkg[0] : event.data.data.pkg
+      const pkg = pkgArg ? pkgArg.trim() : ''
       if(!pkg) {
         craie.info(red('⚠️ Failed'), craie.red(` Missing package name`))
         return
